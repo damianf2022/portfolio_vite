17468df527c0e4740df1c1438f17220a6935cc0a
damianf2022 <damianf2022@gmail.com>
2023-01-23 00:40:35 -0300

    empezando portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Home = () => {
+    return (
+        <div id='content' className=''>
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Proyect.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Proyect = props => {
+  return (
+    <div>Proyect</div>
+  )
+}
+
+Proyect.propTypes = {}
+
+export default Proyect
