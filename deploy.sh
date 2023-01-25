b142c89234a8cbf0547deb4fcabe734c13ccc0cd
damianf2022 <damianf2022@gmail.com>
2023-01-25 15:36:19 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:damianf2022/iniciando_vite.git main:gh-pages
+ git push -f git@github.com:damianf2022/portfolio_vite.git main:gh-pages
 
 cd -
